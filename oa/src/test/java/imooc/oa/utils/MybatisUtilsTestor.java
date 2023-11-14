801a838ccec6eb57c379fb308f7d06d7d4ace0be
littlebear001 <95686003+littlebear001@users.noreply.github.com>
2023-11-14 20:12:16 +0800

    Add files via upload

---
+++ oa/src/test/java/imooc/oa/utils/MybatisUtilsTestor.java
@@ -0,0 +1,20 @@
+package imooc.oa.utils;
+
+import com.imooc.oa.utils.MybatisUtils;
+import org.junit.Test;
+
+public class MybatisUtilsTestor {
+    @Test
+    public void testcase1(){
+        String result = (String) MybatisUtils.executeQuery(sqlSession -> {
+            String out  = (String) sqlSession.selectOne("test.sample");
+            return out;
+        });
+        System.out.println(result);
+    }
+    @Test
+    public void testcase2(){
+        String result = (String) MybatisUtils.executeQuery(sqlSession -> sqlSession.selectOne("test.sample"));
+        System.out.println(result);
+    }
+}
